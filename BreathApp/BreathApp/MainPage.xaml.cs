410a84e5725c065ba1518c665c95c96c6e2901ed
Karthik Sriram <karthiksriramuoa@github.com>
2019-04-18 15:50:53 +1200

    BreathingApparatus app

---
+++ BreathApp/BreathApp/MainPage.xaml.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+
+namespace BreathApp
+{
+    public partial class MainPage : ContentPage
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+        private async void Button_Clicked(object sender, EventArgs e)
+        {
+            await Navigation.PushAsync(new LearnMore());
+        }
+
+        private async void Button_FunctionCheck(object sender, EventArgs e)
+        {
+            await Navigation.PushAsync(new FunctionCheck());
+        }
+    }
+}
